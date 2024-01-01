5827e8ab12f7f26dfeb16a374eb7dbee6f977944
Shivam Mishra <shivaymishraeee@gmail.com>
2024-01-02 00:56:24 +0530

    Hero page is Done right now

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { Suspense, useState, useEffect } from "react";
+
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, meshBounds, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
+
+import * as THREE from "three";
 
 const Computers = () => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={5.15} groundColor="black" />
+      <pointLight intensity={2} />
+      <spotLight
+        position={[-20,50,10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapWidth={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.7, -1.5]}
+        rotation={[-0.01,-0.2,-0.1]}
+      />
+    </mesh>
   )
 }
 
-export default Computers
+const ComputerCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls 
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+      <Preload all />
+    </Canvas >
+  )
+}
+export default ComputerCanvas;
+// export default Computers;
