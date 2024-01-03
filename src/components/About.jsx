55711e9b55ff838ea73f425efb0e7ac610e9bdac
Shivam Mishra <shivaymishraeee@gmail.com>
2024-01-03 23:08:31 +0530

    Some changes in About.js file

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,18 +5,21 @@ import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
 import { services } from '../constants';
-import {fadeIn , textVariant} from '../utils/motion';
+import { fadeIn, textVariant } from '../utils/motion';
 
 const About = () => {
   return (
     <>
-    <motion.div variants={textVariant()}>
-      <p className={styles.sectionSubText}>Introduction</p>
-      <h2 className={styles.sectionHeadText}>Overview.</h2>
-    </motion.div>
-    <motion.p variants={fadeIn("","",0.1,0.1)}>
-      I am a Full Stack Developer with a passion for creating and developing web applications. I have experience in building websites and web applications using JavaScript, React, Node.js, Express, MongoDB, and PostgreSQL. I am a fast learner and a team player who is proficient in an array of scripting languages and multimedia web tools.
-    </motion.p>
+      <motion.div
+        variants={textVariant()}
+        className='mt-4 text-secondary text-[17px] leading-[30px] max-w-3xl'
+      >
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p variants={fadeIn("", "", 0.1, 0.1)}>
+        I am a Full Stack Developer with a passion for creating and developing web applications. I have experience in building websites and web applications using JavaScript, React, Node.js, Express, MongoDB, and PostgreSQL. I am a fast learner and a team player who is proficient in an array of scripting languages and multimedia web tools.
+      </motion.p>
     </>
   )
 }
