dcbd166695cf7c89ec95a94b0a76a42531d814c0
Shivam Mishra <shivaymishraeee@gmail.com>
2024-01-05 01:23:42 +0530

    This portfolio is done now

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,9 +16,120 @@ const Contact = () => {
     message: "",
   });
 
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    const { target } = e;
+    const { name, value } = target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        'service_vzhk6jh',
+        'template_1xdfq2x',
+        {
+          from_name: form.name,
+          to_name: "Shivam Mishra",
+          from_email: form.email,
+          to_email: "shivammishraeee456@gmail.com",
+          message: form.message,
+        },
+        'Z5OIjhM_3D2KpzqxP'
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
+
+          alert("Ahh, something went wrong. Please try again.");
+        }
+      );
+  };
+
   return (
-    <div>Contact</div>
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden ">
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className=" flex-[0.75] bg-black-100 p-8 rounded-2xl "
+      >
+        <p className={styles.sectionSubText}>Get In Touch</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-8 mt-12"
+        >
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4 ">Your Name</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's your name?"
+              className=" bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium "
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4 ">Your Email</span>
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="What's your email?"
+              className=" bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium "
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4 ">Your Message</span>
+            <textarea
+              rows='7'
+              name="message"
+              value={form.message}
+              onChange={handleChange}
+              placeholder="What do you want to say?"
+              className=" bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium "
+            />
+          </label>
+          <button
+            type="submit"
+            className="bg-tertiary py-3 px-8 outline-none text-white font-bold w-fit shadow-md rounded-lg shadow-primary"
+          >
+            {loading ? "Sending..." : "Send"}
+          </button>
+        </form>
+      </motion.div>
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className=" xl:flex-1 xl:h-auto md:h-[550px] h-[350px] "
+      >
+        <EarthCanvas />
+      </motion.div>
+    </div>
   )
 }
 
-export default Contact
+
+const ContactSection = SectionWrapper(Contact, "contact");
+export default ContactSection;
